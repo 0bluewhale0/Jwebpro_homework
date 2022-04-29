dab6d38ad3de3eb8cf72c52cb2d10446ca569654
0bluewhale0 <1312845332@qq.com>
2022-04-30 00:20:28 +0800

    4.30号更新 可以左右下移

--- Tetris/src/Tetris/Tetris.java
+++ Tetris/src/Tetris/Tetris.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class Tetris extends JFrame implements KeyListener {
     //游戏的行数26和列数12
@@ -13,10 +14,21 @@ public class Tetris extends JFrame implements KeyListener {
     JTextArea[][] text;
     //二维矩阵，存储有没有方块
     int [][] data;
+    //存放所有方块的数组：
+    int [] allRect;
+    //存放当前方块：
+    int rect;
     //显示游戏状态的标签
     JLabel label1;
     //显示游戏分数的标签
     JLabel label;
+    boolean isRunning;
+    //方块坐标
+    int x=0,y=0;
+    //线程的休眠时间
+    int time=1000;
+    //得分
+    int score=0;
     public void initWindow(){
         //设置窗口大小
         this.setSize(600,850);
@@ -79,6 +91,7 @@ public class Tetris extends JFrame implements KeyListener {
     }
 
     public Tetris(){//构造函数
+        isRunning=true;//放的位置有影响吗？？
         text = new JTextArea[game_x][game_y];
         data = new int[game_x][game_y];
         //初始化状态和分数标签
@@ -87,11 +100,228 @@ public class Tetris extends JFrame implements KeyListener {
         initGamePanel();
         initExplainPanel();
         initWindow();
+        //初始化方块数组
+        allRect=new int[]{0x00cc,0x8888,0x000f,0x888f,0xf888,0xf111,0x111f,0x0eee,0xffff
+                ,0x0008,0x0888,0x000e,0x0088,0x000c,0x08c8,0x00e4,0x04c4,0x004e,0x08c4, 0x006c,0x04c8,0x00c6};
     }
 
     public static void main(String[] args) {
+
         Tetris tetris=new Tetris();
+        tetris.game_begin();
     }
+
+    public void game_begin()
+    {
+        while(true)
+        {
+            if(!isRunning)//不跑了
+            {
+                break;
+            }
+            //让游戏继续进行
+            game_run();
+        }
+        label1.setText("游戏状态：游戏结束");//更改游戏状态
+    }
+    //随机生成下落方块的方法
+    public void randomRect(){
+        Random random=new Random();
+        rect=allRect[random.nextInt(22)];//生成一个[0-22)的随机数
+    }
+    //游戏运行函数
+    public void game_run()
+    {
+        randomRect();
+        x=0;
+        y=5;
+        for(int i=0;i<game_x;i++)
+        {
+            try{
+                Thread.sleep(time);
+                //如果方块还可以下落
+                if(canFall(x,y))
+                {
+                    //层数++
+                    x++;
+                    fall(x,y);
+                }
+                else
+                {
+                    //将data置为1
+                    changeData(x,y);
+                    //循环遍历4层，看是否可以消除行
+                    for(int j=x;j<x+4;j++)
+                    {
+                        int sum=0;//统计有多少列有方块
+                        for(int k=1;k<game_y-1;k++)
+                        {
+                            if(data[j][k]==1)
+                                sum++;
+                        }
+                        //判断是否有一行可以消除
+                        if(sum==game_y-2)
+                        {
+                            //消除这一行
+                            removeRow(j);
+                        }
+                    }
+                    //判断是否失败
+                    for(int j=1;j<game_y-1;j++)
+                    {
+                        if(data[3][j]==1)
+                        {
+                            isRunning=false;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+//判断是否可以下降
+    public boolean canFall(int m,int n) {
+        int temp=0x8000;
+        //遍历整个方格
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                if((temp & rect)!=0)//有方块
+                {
+                    //判断下一行是否有方块
+                    if(data[m+1][n]!=0)
+                        return false;
+                }
+                n++;
+                temp>>=1;
+            }
+            m++;
+            n=n-4;
+        }
+        //可以下落
+        return true;
+    }
+//改变不可下降的方块对应的区域值的方法
+    public void changeData(int m,int n)
+    {
+        int temp=0x8000;
+        //遍历整个方块
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                if((temp&rect)!=0)
+                {
+                    data[m][n]=1;
+                }
+                n++;
+                temp>>=1;
+            }
+            m++;
+            n=n-4;
+        }
+    }
+
+//移除某一行所有的方块，并且让上面的方块都掉落
+    public void removeRow(int row)
+    {
+        int temp=100;
+        for(int i=row;i>=1;i--)
+        {
+            for(int j=1;i<=game_y-2;j++)
+            {
+                //覆盖
+                data[i][j]=data[i-1][j];
+            }
+        }
+        //刷新游戏区域
+        reflesh(row);
+
+        //方块加速
+        if(time>temp)
+        {
+            time-=temp;
+        }
+        score+=temp;
+        //显示变化后的分数
+        label.setText("游戏得分"+score);
+    }
+
+    //刷新移除某一行之后的游戏界面的方法
+    public void reflesh(int row)
+    {
+        for(int i=row;i>=1;i--)
+        {
+            for(int j=0;j<=game_y-2;j++)
+            {
+                if(data[i][j]==1)//是否是方块
+                {
+                    text[i][j].setBackground(Color.white);
+                }
+                else
+                {
+                    text[i][j].setBackground(Color.pink);
+                }
+            }
+        }
+    }
+//方块向下掉一层
+    public void fall(int m,int n)
+    {
+        if(m>0)
+        {
+            //清除上一层的方块
+            clear(m-1,n);
+        }
+        //重新绘制
+        draw(m,n);
+    }
+
+// 清除上一层掉落后，有颜色的方块的方法
+    public void clear(int m,int n)
+    {
+        int temp=0x8000;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                if((temp&rect)!=0&&m!=0)
+                {
+                    text[m][n].setBackground(Color.pink);
+                }
+                n++;
+                temp>>=1;
+            }
+            m++;
+            n-=4;
+        }
+    }
+
+   //重新绘制掉落后的方块
+    public void draw(int m,int n)
+    {
+        int temp=0x8000;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                if((temp&rect)!=0)
+                {
+                    text[m][n].setBackground(Color.WHITE);
+                }
+                n++;
+                temp>>=1;
+            }
+            m++;
+            n-=4;
+        }
+    }
+//
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -100,6 +330,102 @@ public class Tetris extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+    //控制方块左移
+        if(e.getKeyCode()==37)//左移箭头
+        {
+            if(isRunning==false)
+            {
+                return;
+            }
+            //判断是否碰墙了
+            if(y<=1)
+            {
+                return;
+            }
+            //判断左边是否碰到了别的方块
+            int temp=0x8000;
+            for(int i=x;i<x+4;i++)
+            {
+                for(int j=y;j<y+4;j++)
+                {
+                    if((temp&rect)!=0)
+                    {
+                        if(data[i][j-1]==1)
+                            return;
+                    }
+                    temp>>=1;
+                }
+            }
+            //这时方块可以左移了
+            clear(x,y);
+            draw(x,--y);
+        }
+    //控制方块右移
+        if(e.getKeyCode()==39)
+        {
+            if(isRunning==false)
+            {
+                return;
+            }
+            //判断是否碰到右墙壁
+            //首先获取最右边的边界
+            int temp=0x8000;
+            int m=x;int n=y;
+            int num=1;
+            for(int i=0;i<4;i++)
+            {
+                for(int j=0;j<4;j++)
+                {
+                    if((temp&rect)!=0)
+                    {
+                        if(n>num)
+                        {
+                            num=n;
+                        }
+                    }
+                    n++;
+                    temp>>=1;
+                }
+                m++;n-=4;
+            }
+            if(num>=game_y-2)
+            {
+                return;
+            }
+            //判断右边是否碰到了别的方块
+            temp=0x8000;
+            for(int i=x;i<x+4;i++)
+            {
+                for(int j=y;j<y+4;j++)
+                {
+                    if((temp&rect)!=0)
+                    {
+                        if(data[i][j+1]==1)
+                            return;
+                    }
+                    temp>>=1;
+                }
+            }
+            //这时方块可以左移了
+            clear(x,y);
+            draw(x,++y);
+        }
+    //控制方块下落
+        if(e.getKeyCode()==40)//方向键 下
+        {
+            if(isRunning==false)
+            {
+                return;
+            }
+            //判断还能接着下落吗
+            if(!canFall(x,y))
+            {
+                return;
+            }
+            clear(x,y);
+            draw(++x,y);
+        }
+
 
     }
 
